e32c125972c858a72fb2d24243eb3ec4df93824c
Maciej Bratko <157412862+MaciejBratko@users.noreply.github.com>
2024-11-19 10:55:43 +0100

    Initial commit

---
+++ src/components/App/App.jsx
@@ -0,0 +1,9 @@
+// import Profile from "./components/Profile/Profile";
+// import userData from "./userData.json";
+import css from "./App.module.css";
+
+const App = () => {
+  return <></>;
+};
+
+export default App;
