a14bfb92cef91e87b6c8c150f0f3484612c53388
Maciej Bratko <157412862+MaciejBratko@users.noreply.github.com>
2024-12-30 13:19:48 +0000

    redux removal

--- src/index.jsx
+++ src/index.jsx
@@ -1,15 +1,11 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { Provider } from 'react-redux';
-import { store } from './redux/store';
 import ModernCV from './components/ModernCV';
 import './index.css';
 
 const root = createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <ModernCV />
-    </Provider>
+    <ModernCV />
   </React.StrictMode>
 );
